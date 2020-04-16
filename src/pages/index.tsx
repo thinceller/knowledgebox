cf5e3356894e73c7b115c13254863ebd62a79acb
thinceller <thinceller@gmail.com>
2020-04-16 12:51:02 +0900

    Fix index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import { NextPage, GetServerSideProps } from 'next'
 import fetch from 'node-fetch'
 
 const Index: NextPage<any> = ({ data }) => (
-  <div>{data}</div>
+  <div>{JSON.stringify(data)}</div>
 )
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const res = await fetch('http://localhost:1323')
-  const data = await res.text()
+  const data = await res.json()
 
   return { props: { data } }
 }
