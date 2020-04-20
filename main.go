babc09242f42aba51cba3a16f1246203ca099388
thinceller <thinceller@gmail.com>
2020-04-20 14:26:39 +0900

    Divide logic from main.go

--- main.go
+++ main.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
+
 	"github.com/thinceller/knowledgebox/backend/infra"
+	"github.com/thinceller/knowledgebox/backend/injector"
 )
-
-type PostRequest struct {
-	Title string `json:"title"`
-}
-
-type PostResponse struct {
-	Message string `json:"message"`
-}
-
-type Page struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
-}
-
-type Pages []Page
 
 func main() {
 	e := echo.New()
 
+	// TODO: db を flag などで切り替えられるように変更
 	sqlHandler := infra.NewMySQLHander()
 
 	err := sqlHandler.DB.Ping()
@@ -33,28 +20,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	e.GET("/", func(c echo.Context) error {
-		var pages Pages
-		if err := sqlHandler.DB.Select(&pages, "SELECT id, title FROM pages"); err != nil {
-			return err
-		}
-		return c.JSON(http.StatusOK, pages)
-	})
-	e.POST("/pages", func(c echo.Context) error {
-		req := new(PostRequest)
-		err := c.Bind(req)
-		if err != nil {
-			return err
-		}
+	pageHandler := injector.InjectDBtoHandler(sqlHandler.DB)
 
-		_, err = sqlHandler.DB.Exec("INSERT INTO pages (title) VALUES (?)", req.Title)
-		if err != nil {
-			return err
-		}
-		res := &PostResponse{
-			Message: "created.",
-		}
-		return c.JSON(http.StatusCreated, res)
-	})
+	// routing 設定
+	e.GET("/pages", pageHandler.Index)
+	e.POST("/pages", pageHandler.Create)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
