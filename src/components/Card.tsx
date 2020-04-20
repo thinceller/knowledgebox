9de9f1ba38a8fb5941bc36ca47aa0781ee0f171d
thinceller <thinceller@gmail.com>
2020-04-20 22:30:58 +0900

    Update index page
    * Add first Layout component
    * Add card component for index page

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MCard from '@material-ui/core/Card'
+
+type CardProps = {
+  title: string
+}
+
+export const Card: React.FC<CardProps> = ({ title }) => (
+  <MCard>
+    {title}
+  </MCard>
+)
---
+++ src/models/Page.ts
@@ -0,0 +1,7 @@
+import { Line } from './Line'
+
+export type Page = {
+  id: string
+  title: string
+  lines: Line[]
+}
