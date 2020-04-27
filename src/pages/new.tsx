c5d132f7f2a2fc70e0df95a405102e72a633a90d
thinceller <thinceller@gmail.com>
2020-04-27 17:31:42 +0900

    Fix new page error

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -3,10 +3,27 @@ import { NextPage } from 'next'
 
 import { Layout } from '../components/Layout'
 import { Editor } from '../components/Editor'
+import { Page } from '../models/Page'
 
-const Index: NextPage = () => (
+/* eslint-disable @typescript-eslint/camelcase */
+const createEmptyPage = (): Page => ({
+  id: null,
+  title: '',
+  lines: [
+    {
+      id: undefined,
+      body: '',
+      page_id: null,
+      page_index: 0,
+    },
+  ],
+})
+/* eslint-enable @typescript-eslint/camelcase */
+
+const New: NextPage = () => (
   <Layout>
-    <Editor>{'new page'}</Editor>
+    <Editor pageData={createEmptyPage()} />
   </Layout>
 )
-export default Index
+
+export default New
